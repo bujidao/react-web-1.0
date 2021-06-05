68b4cf05d81ccaa42e6a7fc9e48f9f3b9ab5012a
AlexCeng <izengjing.cn@gmail.com>
2021-06-05 18:21:37 +0800

    update

--- src/layouts/AppHeader/index.tsx
+++ src/layouts/AppHeader/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import styles from './index.less'
-import { history } from 'umi'
-import {
-  MentItemType
-} from './index.d'
+import React from 'react';
+import styles from './index.less';
+import { history } from 'umi';
+import { MentItemType } from './index.d';
+
+import { ReactComponent as Logo } from '../../icons/svg/react.svg';
 
 /**
  * 数据列表
@@ -11,45 +11,42 @@ import {
 const menuListData = [
   {
     label: 'Home',
-    linkTo: '/home'
+    linkTo: '/home',
   },
   {
     label: 'About',
-    linkTo: '/about'
+    linkTo: '/about',
   },
   {
     label: 'User',
-    linkTo: '/user'
-  }
-]
+    linkTo: '/user',
+  },
+];
 
 /**
  * 单个组件
  */
 class MenuListItem extends React.Component<MentItemType> {
-
   constructor(props: any) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    history.push(this.props.itemInfo.linkTo)
+    history.push(this.props.itemInfo.linkTo);
   }
 
   render() {
-    return (
-      <li onClick={this.handleClick}>{this.props.itemInfo.label}</li>
-    )
+    return <li onClick={this.handleClick}>{this.props.itemInfo.label}</li>;
   }
 }
 
 /**
  * 数据循环拼接
  */
-const menuListItems = menuListData.map(item => {
-  return <MenuListItem itemInfo={item} key={item.linkTo}/>
-})
+const menuListItems = menuListData.map((item) => {
+  return <MenuListItem itemInfo={item} key={item.linkTo} />;
+});
 
 /**
  * 最终的header组件
@@ -58,13 +55,13 @@ class AppHeader extends React.Component {
   render() {
     return (
       <div className={styles.header}>
-        <div className="logo">Logo</div>
-        <ul>
-          {menuListItems}
-        </ul>
+        <div className="logo">
+          <Logo width={90} height={120} className={styles.mylogo} />
+        </div>
+        <ul>{menuListItems}</ul>
       </div>
-    )
+    );
   }
 }
 
-export default AppHeader
+export default AppHeader;
